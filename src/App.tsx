db5250caca7516bfa14be4a0a3f634c98583b2e8
mustafa-khann <mkafridi073@gmail.com>
2025-11-24 14:02:12 +0500

    Add Vercel Analytics import and component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import PrivacyPolicy from './pages/PrivacyPolicy'
 import TermsOfService from './pages/TermsOfService';
+import { Analytics } from "@vercel/analytics/next";
 import DeleteData from './pages/DeleteData';
 import DeleteAccount from './pages/DeleteAccount';
 
@@ -15,9 +16,9 @@ function App() {
         <Route path="/delete-data" element={<DeleteData />} />
         <Route path="/delete-account" element={<DeleteAccount />} />
       </Routes>
+      <Analytics />
     </Router>
   )
 }
 
 export default App
-
